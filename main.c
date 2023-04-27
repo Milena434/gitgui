e4f423e334e6e1dce3979b04d446ecc3a89517cc
Milena434 <milenasokolova14@gmail.com>
2023-04-27 21:52:51 +0300

    Changed the phrase

--- main.c
+++ main.c
@@ -3,36 +3,6 @@ using namespace std;
 
 int main ()
 {
-<<<<<<< HEAD
 	setlocale (LC_ALL, "Russian");
-	cout << "Здравствуй, друг";
+	cout << "Helllo";
 	return 0;
-=======
-    setlocale (LC_ALL, "Russian");
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of a1e367c (Revert "Changed an exiting file")
-    cout << "Hello, appwes!" << endl;
-=======
-    cout << "Hello, appews!" << endl;
->>>>>>> d6ea8e7e05f6ebfd2f28b2e8640c06e290ebe024
-<<<<<<< HEAD
-=======
-    cout << "Hello, appes!" << endl;
->>>>>>> parent of d6ea8e7 (Changed an exiting file)
-=======
-    cout << "Hello, appes!" << endl;
->>>>>>> parent of d6ea8e7 (Changed an exiting file)
-=======
-    cout << "Hello, appes!" << endl;
->>>>>>> parent of d6ea8e7 (Changed an exiting file)
-=======
->>>>>>> parent of a1e367c (Revert "Changed an exiting file")
-  
-    return 0;
->>>>>>> 97668b18a6702cdde16092337acb6e3d74a5b0c4
-}
